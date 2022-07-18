465f52a6e8bb5ba3e34cc6fb07ef42e92c94c8ab
Kimari <grgkimari@gmail.com>
2022-07-18 22:38:51 +0300

    created home app

--- msdms/apps/accounts/models.py
+++ msdms/apps/accounts/models.py
@@ -1,5 +1,3 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-class CustomUser(AbstractUser):
-    accountType = models.CharField(verbose_name="accountType", blank = False, null=True)
--- msdms/msdms/settings.py
+++ msdms/msdms/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    '',
+    'apps.accounts',
+    'apps.home',
 ]
 
 MIDDLEWARE = [
