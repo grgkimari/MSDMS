48674178ab1b8fe8f5d859885ace18ff9b9ab0b1
Kimari <grgkimari@gmail.com>
2022-07-20 12:43:01 +0300

    refactored files

--- msdms/apps/accounts/models.py
+++ msdms/apps/accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(blank=False, null = False, verbose_name="email",unique=True)
+    user_type = models.CharField(blank = False, null = False, verbose_name = "usertype", max_length=25)
+    date_joined = models.DateField(auto_now_add=True)
+
+    USERNAME_FIELD: 'email'
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
