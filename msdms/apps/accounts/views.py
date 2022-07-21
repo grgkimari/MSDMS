22c761ff8827021561433d80d9ca1c77a0cb475c
Kimari <grgkimari@gmail.com>
2022-07-21 22:41:05 +0300

    user registration

--- msdms/apps/accounts/views.py
+++ msdms/apps/accounts/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import  AuthenticationForm
 
 def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request.POST)
+        user = form.get_user()
+        login(request, user)
+        return redirect ('homepage')
     return render(request, 'login.html')
 
 def register_view(request):
+    
     return render(request, 'register.html')
