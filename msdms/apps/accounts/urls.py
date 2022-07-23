21330899e6594d5a6098a488311aeeb2e99bc537
Kimari <grgkimari@gmail.com>
2022-07-23 20:45:55 +0300

    Implemented login/logout

--- msdms/apps/accounts/urls.py
+++ msdms/apps/accounts/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('login',views.login_view,name = 'login'),
     path('register',views.register_view,name = 'register'),
+    path('logout', views.logout_view, name="logout")
 ]
--- msdms/apps/accounts/views.py
+++ msdms/apps/accounts/views.py
@@ -1,14 +1,46 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import  AuthenticationForm
+from multiprocessing import context
+from django.shortcuts import render, redirect
+from .forms import SignupForm, LoginForm
+from django.contrib.auth import authenticate,login,logout
 
 def login_view(request):
     if request.method == 'POST':
-        form = AuthenticationForm(request.POST)
-        user = form.get_user()
-        login(request, user)
-        return redirect ('homepage')
-    return render(request, 'login.html')
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(email = form.cleaned_data.get('email'), password = form.cleaned_data.get('password'))
+            if user:
+                login(request, user)
+                return redirect('homepage')
+            else:
+                msg = "Invalid credentials"
+        else:
+            msg = "Form is not valid."
+
+
+        context = {
+            'msg' : msg
+        }
+        
+    else:
+        return render(request, 'accounts/login.html')
+    
 
 def register_view(request):
-    
-    return render(request, 'register.html')
+    msg = None
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        else:
+            msg = "Incorrect login details"
+            context = {
+                'msg' : msg
+            }
+            return render(request,'register.html', context)
+    else:
+        return render(request, 'register.html', context)
+
+def logout_view(request):
+    logout(request)
+    return redirect('homepage')
